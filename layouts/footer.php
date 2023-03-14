f3663572087a9bda55527dc41d0c93c53aab35c4
apriliatrijaldi <127570384+apriliatrijaldi@users.noreply.github.com>
2023-03-14 10:40:22 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer py-2 bg-light ">
+  <div class="container">
+    <p class="text-center">UKK RPL 2023 | Aprilia trijaldi | SMK MAHARDHIKA</p>
+  </div>
+
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
